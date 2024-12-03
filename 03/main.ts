da1ac48e1a96c9b429ad05bcddd81afc4d5f9b13
Jimmy Mullen <jmullen1@chewy.com>
2024-12-03 08:07:52 -0500

    day 3

---
+++ 03/main.ts
@@ -0,0 +1,35 @@
+import { loadFile } from "../utils/utils.ts";
+
+const one = async () => {
+  const data = await loadFile("input.txt");
+  const str = data.join();
+  const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+  const result = [...str.matchAll(regex)].reduce((acc, d) => {
+    const [_, x, y] = d;
+    return acc += parseInt(x) * parseInt(y);
+  }, 0);
+  
+  console.log("one", result);
+}
+
+one();
+
+const two = async () => {
+  const data = await loadFile("input.txt");
+  const str = data.join();
+  const donts = str.split("don't()");
+  const doString = donts.reduce((acc, d) => {
+    const dos = d.split("do()").slice(1).join();
+    return acc + dos;
+  }, donts[0]);
+
+  const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+  const result = [...doString.matchAll(regex)].reduce((acc, d) => {
+    const [_, x, y] = d;
+    return acc += parseInt(x) * parseInt(y);
+  }, 0);
+  
+  console.log("two", result);
+}
+
+two();
