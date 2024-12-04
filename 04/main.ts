a9a3719c8d392472318521e385e976f290014e9c
Jimmy Mullen <jmullen1@chewy.com>
2024-12-04 07:19:27 -0500

    day 4, first try baby

---
+++ 04/main.ts
@@ -0,0 +1,41 @@
+import { loadFile } from "../utils/utils.ts";
+
+const xmasCount = (matrix:string[][], x:number, y:number) => {
+  if (matrix[x][y] !== "X") return 0;
+  let count = 0;
+  for (const xo of [-1, 0, 1]) {
+    for (const yo of [-1, 0, 1]) {
+      try {
+        if (
+          matrix[x + xo][y + yo] === "M" &&
+          matrix[x + xo*2][y + yo*2] === "A" &&
+          matrix[x + xo*3][y + yo*3] === "S"
+        ) count++;
+      } catch (e) {
+        // console.log("can't access", xo, yo)
+      }
+    }
+  }
+  return count;
+}
+
+const one = async () => {
+  const data = await loadFile("input.txt");
+  const matrix = data.reduce<string[][]>((acc, d) => acc.concat([d.split("")]), [])
+  let result = 0;
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix[x].length; y++) {
+      result += xmasCount(matrix, x, y);
+    }
+  }
+  console.log("one", result);
+}
+
+const two = async () => {
+  const result = 0
+  
+  console.log("two", result);
+}
+
+one();
+two();
