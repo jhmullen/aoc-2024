43bd8ce6cfb96913fc139b42f61c59e798ae7e1c
Jimmy Mullen <jmullen1@chewy.com>
2024-12-11 11:52:31 -0500

    sheesh part 2

--- 11/main.ts
+++ 11/main.ts
@@ -1,5 +1,17 @@
+import { totalmem } from "node:os";
 import { loadFile } from "../utils/utils.ts";
 import process from "node:process";
+
+const blinkMap = {} as any;
+
+const doBlink = (digit:number) => {
+  if (digit === 0) return [1];
+  if ((""+digit).length % 2 === 0) {
+    const str = "" + digit;
+    return [str.slice(0, str.length / 2), str.slice(str.length / 2)].map(Number); 
+  }
+  return [digit * 2024];
+}
 
 const one = async () => {
   
@@ -8,21 +20,6 @@ const one = async () => {
   
   let i = 0;
   const BLINKS = 25;
-
-  const doBlink = (digit:number) => {
-    if (digit === 0) return [1];
-    if ((""+digit).length % 2 === 0) {
-      const str = "" + stones[i];
-      return [str.slice(0, str.length / 2), str.slice(str.length / 2)].map(Number); 
-    }
-    return [digit * 2024];
-  }
-
-  const proc = (digit:number) => {
-    const start = [digit];
-
-    return start;
-  }
 
   for (let blink = 0; blink < BLINKS; blink++) {
     process.stdout.write(`Processing: ${Math.round((blink / BLINKS) * 100)}%\r`);
@@ -39,11 +36,39 @@ const one = async () => {
   console.log("one", result);
 }
 
-const two = async () => {
-  const result = 0;
+const BLINKS = 75;
+
+const map = {} as any;
+
+const doBlink2 = (digit:number, blinks:number, total:number = 0):number => {
+  if (blinks === 0) return total;
+  const key = `${digit}.${blinks}`;
+  if (map[key]) return map[key];
   
-  // console.log("two", result);
+  process.stdout.write(`Processing: ${Math.round(((BLINKS-blinks) / BLINKS) * 100)}%\r`);
+  const [yes, maybe] = doBlink(digit);
+  
+  // console.log(`blink: ${blinks}, digit: ${digit} => ${yes}, ${maybe}`);
+  const result = doBlink2(yes, blinks-1, 1) + (maybe !== undefined ? doBlink2(maybe, blinks - 1, total) : 0);
+  map[key] = result;
+  return result
 }
 
-one();
+const two = async () => {
+  const data = await loadFile("input.txt");
+  const stones = data[0].split(" ").map(Number)
+  
+  let i = 0;
+  let result = 0;
+
+  do {
+    console.log("STEP", i);
+    result += doBlink2(stones[i], BLINKS);
+    i++;
+  } while (i < stones.length);
+
+  console.log("two", result);
+}
+
+// one();
 two();
